ac4d1d18ad19753b96df01e90897f9dea785ebb3
sk-repository <34868573+sk-repository@users.noreply.github.com>
2020-08-15 15:49:03 +0200

    Add files via upload
    first commit

---
+++ udpProxy.py
@@ -0,0 +1,35 @@
+#
+#   PxVS-Frames Duplicator
+#   s.kloniecki@outlook.com
+#
+import socket
+
+# set these values
+bufsize = 8192
+target_host = "127.0.0.1"
+target_port1 = 9002
+target_port2 = 9003
+listen_host = "192.168.204.49"
+listen_port = 9001
+
+
+def forward(data, port):
+    print("Forwarding: '%s' from port %s" % (data, port))
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(("localhost", port))                          # Bind to the port data came in on 9001
+    sock.sendto(data, (target_host, target_port1))          # Send data to 9002
+    sock.sendto(data, (target_host, target_port2))          # Send data to 9003
+
+
+def listen(host, port):
+    listen_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    listen_socket.bind((host, port))
+    print("*** Listening on %s:%s" % (host, port))
+    while True:
+        data, addr = listen_socket.recvfrom(bufsize)
+        forward(data, addr[1])                              # data and port1
+
+
+# main
+listen(listen_host, listen_port)
+
